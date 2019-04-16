8700c554e684b71916f285efc4bd66569d9a2aaf
Chris Anderson <christopher.anderson@gmail.com>
2019-04-16 17:08:01 -0400

    Add .gitignore

---
+++ CollectionScrollCrash/Font.swift
@@ -0,0 +1,9 @@
+//
+//  Font.swift
+//  CollectionScrollCrash
+//
+//  Created by Chris Anderson on 4/16/19.
+//  Copyright © 2019 Polutropos. All rights reserved.
+//
+
+import Foundation
---
+++ CollectionScrollCrash/ProductCell.swift
@@ -0,0 +1,9 @@
+//
+//  ProductCell.swift
+//  CollectionScrollCrash
+//
+//  Created by Chris Anderson on 4/16/19.
+//  Copyright © 2019 Polutropos. All rights reserved.
+//
+
+import Foundation
